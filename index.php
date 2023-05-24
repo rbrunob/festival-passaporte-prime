9891ec7a6df77fd9dae1531596fcd1b305815122
rbrunob <bruno.bragaramos@gmail.com>
2023-05-24 10:27:43 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./src/assets/css/main.css" />
+    <link rel="stylesheet" href="./src/assets/css/pages/homepage.css" />
+
+    <title>Festival Passaporte Prime</title>
+</head>
+
+<body>
+
+    <main>
+
+    </main>
+
+    <script src="./src/assets/js/app.js"></script>
+</body>
+
+</html>
