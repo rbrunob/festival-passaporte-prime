fa175665132303f343cf0d26798699d0b135000e
rbrunob <bruno.bragaramos@gmail.com>
2023-05-24 11:49:13 -0300

    commit carousel main

---
+++ src/components/carousel/carousel.php
@@ -0,0 +1,27 @@
+<section id="carousel">
+    <div class="carousel_row">
+        <div class="carousel_items">
+            <div class="carousel_item">
+                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-01.webp" alt="image carousel" />
+            </div>
+            <div class="carousel_item">
+                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-01.webp" alt="image carousel" />
+            </div>
+            <div class="carousel_item">
+                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-01.webp" alt="image carousel" />
+            </div>
+            <div class="carousel_item">
+                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-01.webp" alt="image carousel" />
+            </div>
+            <div class="carousel_item">
+                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-01.webp" alt="image carousel" />
+            </div>
+            <div class="effect_carousel">
+                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/golden-glitter.webp" alt="" />
+            </div>
+        </div>
+        <div class="carousel_button">
+            <button>teaser</button>
+        </div>
+    </div>
+</section>
