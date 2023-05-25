b3ec941933e6bfe758768fc2b92fb3cb55788049
rbrunob <bruno.bragaramos@gmail.com>
2023-05-25 08:49:03 -0300

    update carousel done

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,90 @@
-console.log("Hello, World!")
+const slides = document.querySelectorAll('.carousel_item');
+const dynamicBackground = document.querySelector('#carousel');
+let index = 0;
+
+slides.forEach(slide => {
+    slide.addEventListener('click', () => {
+        index = parseInt(slide.getAttribute('data-index'));
+
+        updateSlide();
+        updateBackground();
+    });
+});
+
+const updateBackground = () => {
+    switch (index) {
+        case 0:
+            dynamicBackground.style.backgroundImage = ' url("https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/bg-passaporte-prime-01.webp")'
+            break;
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+            dynamicBackground.style.backgroundImage = ' url("https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/bg-passaporte-prime-02.webp")'
+            break;
+        case 4:
+    }
+}
+
+const updateSlide = () => {
+    for (i = 0; i < slides.length; i++) {
+        if (slides.length == 3) {
+            switch (index) {
+                case 0:
+                    slides[0].classList.replace(`item-${i}`, "item-0");
+                    slides[1].classList.replace(`item-${i}`, "item-1");
+                    slides[2].classList.replace(`item-${i}`, "item-2");
+                    break;
+                case 1:
+                    slides[0].classList.replace(`item-${i}`, "item-2");
+                    slides[1].classList.replace(`item-${i}`, "item-0");
+                    slides[2].classList.replace(`item-${i}`, "item-1");
+                    break;
+                case 2:
+                    slides[0].classList.replace(`item-${i}`, "item-1");
+                    slides[1].classList.replace(`item-${i}`, "item-2");
+                    slides[2].classList.replace(`item-${i}`, "item-0");
+            }
+        } else {
+            switch (index) {
+                case 0:
+                    slides[0].classList.replace(`item-${i}`, "item-0");
+                    slides[1].classList.replace(`item-${i}`, "item-1");
+                    slides[2].classList.replace(`item-${i}`, "item-2");
+                    slides[3].classList.replace(`item-${i}`, "item-3");
+                    slides[4].classList.replace(`item-${i}`, "item-4");
+                    break;
+                case 1:
+                    slides[0].classList.replace(`item-${i}`, "item-2");
+                    slides[1].classList.replace(`item-${i}`, "item-0");
+                    slides[2].classList.replace(`item-${i}`, "item-4");
+                    slides[3].classList.replace(`item-${i}`, "item-1");
+                    slides[4].classList.replace(`item-${i}`, "item-3");
+                    break;
+                case 2:
+                    slides[0].classList.replace(`item-${i}`, "item-1");
+                    slides[1].classList.replace(`item-${i}`, "item-3");
+                    slides[2].classList.replace(`item-${i}`, "item-0");
+                    slides[3].classList.replace(`item-${i}`, "item-4");
+                    slides[4].classList.replace(`item-${i}`, "item-2");
+                    break;
+                case 3:
+                    slides[0].classList.replace(`item-${i}`, "item-4");
+                    slides[1].classList.replace(`item-${i}`, "item-2");
+                    slides[2].classList.replace(`item-${i}`, "item-3");
+                    slides[3].classList.replace(`item-${i}`, "item-0");
+                    slides[4].classList.replace(`item-${i}`, "item-1");
+                    break;
+                case 4:
+                    slides[0].classList.replace(`item-${i}`, "item-3");
+                    slides[1].classList.replace(`item-${i}`, "item-4");
+                    slides[2].classList.replace(`item-${i}`, "item-1");
+                    slides[3].classList.replace(`item-${i}`, "item-2");
+                    slides[4].classList.replace(`item-${i}`, "item-0");
+            }
+        }
+    }
+}
--- src/components/carousel/carousel.php
+++ src/components/carousel/carousel.php
@@ -1,27 +1,40 @@
 <section id="carousel">
     <div class="carousel_row">
         <div class="carousel_items">
-            <div class="carousel_item">
+            <div class="carousel_item item-0" data-index="0">
                 <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-01.webp" alt="image carousel" />
             </div>
-            <div class="carousel_item">
-                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-01.webp" alt="image carousel" />
+            <div class="carousel_item item-1" data-index="1">
+                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-03.webp" alt="image carousel" />
             </div>
-            <div class="carousel_item">
-                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-01.webp" alt="image carousel" />
+            <div class="carousel_item item-2" data-index="2">
+                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-04.webp" alt="image carousel" />
             </div>
-            <div class="carousel_item">
-                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-01.webp" alt="image carousel" />
+            <div class="carousel_item item-3" data-index="3">
+                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-02.webp" alt="image carousel" />
             </div>
-            <div class="carousel_item">
-                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-01.webp" alt="image carousel" />
-            </div>
-            <div class="effect_carousel">
-                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/golden-glitter.webp" alt="" />
+            <div class="carousel_item item-4" data-index="4">
+                <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image-carousel-05.webp" alt="image carousel" />
             </div>
         </div>
+        <div class="carousel_actions">
+            <div class="prev">
+
+            </div>
+            <div class="next">
+
+            </div>
+        </div>
+        <div class="effect_carousel">
+            <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/golden-glitter.webp" alt="" />
+        </div>
         <div class="carousel_button">
-            <button>teaser</button>
+            <button>
+                teaser
+            </button>
+        </div>
+        <div class="light_effect">
+            <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/light-button.webp" alt="" />
         </div>
     </div>
 </section>
