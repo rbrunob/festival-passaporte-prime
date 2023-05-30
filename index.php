859f125b8709997ef605fd8af7c23b8310bad027
rbrunob <bruno.bragaramos@gmail.com>
2023-05-30 09:16:47 -0300

    feat: new section 'channels' -- html structure done

--- index.php
+++ index.php
@@ -13,12 +13,14 @@
 
 <body>
     <? include("./src/components/header/header.php") ?>
+    
     <main>
         <? include("./src/components/carousel/carousel.php") ?>
         <? include("./src/components/sinopse/sinopse.php") ?>
         <? include("./src/components/characters/characters.php") ?>
         <? include("./src/components/gallery/gallery.php") ?>
         <? include("./src/components/xtra/xtra.php") ?>
+        <? include("./src/components/channels/channels.php") ?>
     </main>
 
     <script src="./src/assets/js/app.js"></script>
---
+++ src/components/channels/channels.php
@@ -0,0 +1,48 @@
+<section id="channels">
+    <div class="channels_row">
+        <h1 class="channels_title">Onde assistir</h1>
+        <div class="channels_area">
+            <div class="channel_container">
+                <div class="logo_main">
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/logo-prime-box.webp" alt="Logo Prime Box Brazil" />
+                </div>
+                <p class="description">Em todas as operadoras de tv por assinatura</p>
+                <div class="tv_logo_items">
+                    <div class="item">
+                        <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/logo-claro-tv.webp" alt="Claro tv" />
+                        <p class="name_item">Canal 156 / HD 656</p>
+                    </div>
+                    <div class="item">
+                        <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/logo-vivo.webp" alt="Vivo" />
+                        <p class="name_item">Canal 157</p>
+                    </div>
+                    <div class="item">
+                        <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/logo-sky.webp" alt="Sky" />
+                        <p class="name_item">Canal 109</p>
+                    </div>
+                    <div class="item">
+                        <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/logo-oi.webp" alt="Oi" />
+                        <p class="name_item">Canal 85</p>
+                    </div>
+                </div>
+            </div>
+            <div class="avaliable_container">
+                <div class="avaliable_title_container">
+                    <h2 class="title">Disponível</h2>
+                </div>
+                <div class="avaliable_logos">
+                    <div class="logo">
+                        <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/logo-box-brazil-play.webp" alt="Box Brazil Play" />
+                        <a href="https://www.boxbrazilplay.com.br" class="site">www.boxbrazilplay.com.br</a>
+                        <a href="https://www.boxbrazilplay.com.br" class="button">Assine Aqui!</a>
+                    </div>
+                    <div class="logo">
+                        <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/logo-claro-tv-plus.webp" alt="Claro Tv Plus" />
+                        <a href="https://www.clarotvmais.com.br" class="site">www.clarotvmais.com.br</a>
+                        <a href="https://www.clarotvmais.com.br" class="button">Assine Aqui!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
