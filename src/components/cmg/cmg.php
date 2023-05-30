f9a0afd44f5138d918b4d99c98fee7d8d008b48c
rbrunob <bruno.bragaramos@gmail.com>
2023-05-30 10:20:33 -0300

    feat: title cmg sections

--- src/components/cmg/cmg.php
+++ src/components/cmg/cmg.php
@@ -1,5 +1,6 @@
 <section id="cmg">
     <div class="cmg_row">
+        <h1 class="cmg_title">Parceiro</h1>
         <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/cmg-logo.svg" alt="Logo CMG">
     </div>
 </section>
