e67c29f27a7a7a5ccd6a8656846bf31b5c95cb2d
rbrunob <bruno.bragaramos@gmail.com>
2023-06-02 09:11:23 -0300

    feat: HTML structure done

---
+++ homepage-institucional.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./src/assets/css/main.css" />
+    <link rel="stylesheet" href="./src/assets/css/pages/homepage-institucional.css" />
+
+    <title>Festival Passaporte Prime</title>
+</head>
+
+<body>
+
+    <? include("./src/components/header/header.php") ?>
+
+    <main>
+        
+        <? include("./src/components/channels/channels.php") ?>
+        <? include("./src/components/cmg/cmg.php") ?>
+    </main>
+
+    <? include("./src/components/footer/footer.php") ?>
+
+    <script src="./src/assets/js/app.js"></script>
+</body>
+
+</html>
