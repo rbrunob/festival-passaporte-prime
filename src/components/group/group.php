cefe546a6fcbe52600421335049836376a0e2646
rbrunob <bruno.bragaramos@gmail.com>
2023-06-02 14:43:45 -0300

    feat: HTML structure group done

---
+++ src/components/group/group.php
@@ -0,0 +1,13 @@
+<section id="group">
+    <div class="group_row">
+        <div class="group_content">
+            <h1 class="group_title">O Grupo Box Brazil</h1>
+            <p class="group_description">
+                O Grupo Box Brazil tem foco em entretenimento, tecnologia e inovação, com abrangência nacional e internacional. É formado pela maior programadora independente brasileira, a Box Brazil Channels, com 6 canais Pay TV e 10 canais Streaming, alcançando 42 milhões de pessoas diariamente. E também pela Container Media, que desenvolve e opera Plataformas Streaming e pela Media Mundus, agregadora e distribuidora de conteúdos digitais. Além de sua plataforma de streaming, a Box Brazil Play.
+            </p>
+        </div>
+        <div class="group_image">
+            <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/logo-box-brazil-media-group-blue.svg" alt="Logo Box Brazil Media Group" />
+        </div>
+    </div>
+</section>
