59ed318ac7ac85233326185325d8982577c995bf
rbrunob <bruno.bragaramos@gmail.com>
2023-06-22 10:48:28 -0300

    FEAT: attribute loading lazy to images gallery

--- src/components/gallery/gallery.php
+++ src/components/gallery/gallery.php
@@ -7,52 +7,52 @@
         <div class="gallery_area">
             <div class="gallery_items">
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_0.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_0.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_2.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_2.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_3.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_3.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_4.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_4.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_5.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_5.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_6.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_6.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_7.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_7.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_8.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_8.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_9.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_9.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_10.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_10.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_11.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_11.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_12.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_12.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_13.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_13.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_14.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_14.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_15.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_15.webp" alt="image gallery" loading="lazy" />
                 </div>
                 <div class="gallery_item">
-                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_16.webp" alt="image gallery" />
+                    <img src="https://preprod.containermedia.com.br/passaporte-prime/src/assets/images/image_gallery_16.webp" alt="image gallery" loading="lazy" />
                 </div>
             </div>
         </div>
